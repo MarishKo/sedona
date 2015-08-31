a37402c213e115df693e06898fe0dfc49bb36158
MarishKo <deifos@mail.ru>
2015-08-31 20:21:29 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+﻿var popup = document.querySelector('.search-form'),
+    button = document.querySelector('.js-show');
+
+button.addEventListener('click', function(event) {
+	event.preventDefault();
+ 	popup.classList.toggle('form-show');
+});
